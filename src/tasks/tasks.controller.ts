2012afbab2e7502a58cf0125c37ba6a2f2a788e8
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-01-05 20:00:52 +0100

    create new task endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './task.model.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -9,5 +10,10 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() request: CreateTaskDto): Task {
+    return this.tasksService.createTask(request.title, request.description);
   }
 }
