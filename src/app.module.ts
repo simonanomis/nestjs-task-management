1fb36c9ccd2984a424530d38aac57dabf52f2dc9
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-01-05 19:34:00 +0100

    get all tasks API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
 
 @Module({
   imports: [TasksModule],
   controllers: [TasksController],
+  providers: [TasksService],
 })
 export class AppModule {}
